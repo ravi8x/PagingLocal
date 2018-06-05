696e0cf2a8d01b54775e2a005e36a617833b7967
Ravi Tamada <ravi8x@gmail.com>
2018-06-05 18:47:00 +0530

    new project

---
+++ app/src/main/java/info/androidhive/paging/MainActivity.java
@@ -0,0 +1,20 @@
+package info.androidhive.paging;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import info.androidhive.paging.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/info/androidhive/paging/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package info.androidhive.paging.ui.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
